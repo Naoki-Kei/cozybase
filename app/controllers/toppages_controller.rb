3afb65463dbb9f3a6645daae9cdad1b2642517cd
Naoki.K <nyaoking1012@yahoo.co.jp>
2020-11-26 12:26:22 +0000

    最終課題修正

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,9 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @post = current_user.posts.build  # form_with 用
-      @posts = current_user.feed_posts.order(id: :desc).page(params[:page])
+      # @posts = current_user.feed_posts.order(id: :desc).page(params[:page])
+     
+      @posts = Post.all.page(params[:page]).per(12)
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,23 @@ class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
 
   def index
-    @users = User.order(id: :desc).page(params[:page]).per(25)
+    @users = User.order(id: :desc).page(params[:page]).per(10)
   
   end
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.order(id: :desc).page(params[:page])
+    @posts = @user.posts.order(id: :desc).page(params[:page]).per(8)
     counts(@user)
   end
   
   def edit
     @user = User.find(params[:id])
+     if @user == current_user
+       render "edit"
+     else
+       redirect_to root_url
+     end
 
   end
 
@@ -82,4 +87,14 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation,:comment)
   end
+  
+  def correct_user
+        @user = current_user.users.find_by(id: params[:id])
+        unless @user
+          redirect_to root_url
+        end
+  end
+  
+  
+  
 end
